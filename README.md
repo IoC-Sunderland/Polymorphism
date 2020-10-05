107c5ba18960758385488e4266f2a2656b8141e8
Institute of Coding (Sunderland) <gavin.mcclary@gmail.com>
2020-10-05 14:36:31 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Polymorphism
+Python is implicity polmorphic i.e. functions can take arguments of different t ypes (e.g. list, string, dicts) and still work accordingly.
---
+++ main.py
@@ -0,0 +1,9 @@
+# Polymorphism
+def f(x,y):
+    print("values: ", x, y)
+
+f([3,5,6],(3,5))
+
+f("A String", ("A tuple", "with Strings"))
+
+f({2,3,9}, {"a":3.4,"b":7.8, "c":9.04})
