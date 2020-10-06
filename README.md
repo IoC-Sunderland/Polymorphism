3651f24b408951302d94d8d7a7d57d856fcdfc96
Institute of Coding (Sunderland) <gavin.mcclary@gmail.com>
2020-10-06 17:37:22 +0000

    Added text to README.md

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Polymorphism
 Python is implicity polmorphic i.e. functions can take arguments of different types (e.g. list, string, dicts) and still work accordingly.
 
-They can also behave differently depending on the type of the arguments we provide. 
+They can also behave differently depending on the type of the arguments we provide.
+
+# Examples of Polymorphism in Python
+Method Overloading: 
+
+Operator Overloading:
+
+Method Overriding:
--- main.py
+++ main.py
@@ -7,3 +7,58 @@ f([3,5,6],(3,5))
 f("A String", ("A tuple", "with Strings"))
 
 f({2,3,9}, {"a":3.4,"b":7.8, "c":9.04})
+
+#### Method Overloading
+class Human:
+
+    def sayHello(self, name=None):
+    
+        if name is not None:
+            print('Hello ' + name)
+        else:
+            print('Hello ')
+        
+
+# Create instance
+obj = Human()
+    
+# Call the method
+obj.sayHello()
+    
+# Call the method with a parameter
+obj.sayHello('Gav')
+
+
+### Operator Overloading
+print(4 + 4) # Addition
+print('He' + 'llo') # Concatenation
+
+
+### Method Overriding
+class Parent(): 
+      
+    
+    def __init__(self): 
+        self.value = "Parent's __init__"
+          
+    def show(self): 
+        print(self.value) 
+          
+# Define child class 
+class Child(Parent): 
+      
+    
+    def __init__(self): 
+        self.value = "Child's __init__"
+          
+    
+    def show(self): 
+        print(self.value) 
+          
+          
+
+obj1 = Parent() # Parent's __init__
+obj2 = Child() # Child's __init__
+  
+obj1.show() 
+obj2.show()
